8f85cf4ffff1d91e8a77f0ed9ed2738e26121093
Samuel Novaes <samuel.novaes.96@gmail.com>
2019-04-29 14:32:48 -0300

    Update

--- server/routes/roms.js
+++ server/routes/roms.js
@@ -1,40 +1,37 @@
 const router = require('express').Router()
 const fs = require('fs-extra')
-let listGames = []
+const path = require('path')
 
-router.get("/", (req, res) => {
-    fs.readdir("./static/games/roms", "utf-8", (err, files) => {
-        if (err) {
-            return err.message
-        }
-        files.forEach(cns => {
-            if (err) {
-                return err.message
-            }
-            fs.readdir("./static/games/roms/" + cns, "utf-8", (err, games) => {
-                if (err) {
-                    return err.message
-                }
-                games.forEach(game => {
-                    fs.readdir("./static/games/roms/" + cns + "/" + game, "utf-8", (err, roms) => {
-                        if (err) {
-                            return err.message
-                        }
-                        roms.forEach(rom => {
-                            listGames.push({
-                                cns,
-                                folder: game,
-                                rom
-                            })
-                        })
-                    });
-                });
+router.get("/", async (req, res) => {
+	const listGames = []
 
-            })
-        });
-        res.send(listGames)
-        listGames = []
-    })
+	try {
+		const romsDir = path.join(__dirname, '..', '..', 'static', 'games', 'roms')
+		const files = await fs.readdir(romsDir, "utf-8")
+
+		for(const cns of files){
+			const cnsDir = path.join(romsDir, cns)
+			const games = await fs.readdir(cnsDir, "utf-8")
+
+			for(const game of games){
+				const gameDir = path.join(cnsDir, game)
+				const roms = await fs.readdir(gameDir, "utf-8")
+
+				for(const rom of roms){
+					listGames.push({
+						cns,
+						folder: game,
+						rom
+					})
+				}
+			}
+		}
+
+		res.json(listGames)
+	}
+	catch(err){
+		res.status(500).send(err.message)
+	}
 })
 
 
